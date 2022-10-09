d410366f893e8fa2f5046ef1b8a0d9de1b157bd7
Spencer Lepine <spencerlepine26@gmail.com>
2022-10-09 11:55:10 -0700

    add image functionality to react app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "s3a7a771e3": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
